62655478463fe3a3ea78ff31d0e20c8a97eab396
rirefat <rirefat.official@gmail.com>
2023-03-19 15:57:44 +0600

    prepare json data of portfolios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HeroSection from './components/HeroSection/HeroSection';
+import Portfolio from './components/Portfolio/Portfolio';
 
 const App = () => {
   return (
     <div className='container'>
       <HeroSection></HeroSection>
+      <Portfolio></Portfolio>
     </div>
   );
 };
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Portfolio.css';
+
+const Portfolio = () => {
+    return (
+        <div  className='portfolio-section'>
+            <h3>Works we are proud of</h3>
+            <p>Good Creation Systems Ltd is a fully Digital solution providing company incorporated and registered under the corporate affair commission in July 20, 2022.</p>
+        </div>
+    );
+};
+
+export default Portfolio;
