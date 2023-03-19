fa9ad82ba6cda180a4af53efa1b334cb533528d2
rirefat <rirefat.official@gmail.com>
2023-03-19 23:18:23 +0600

    done right side container of portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import usePortfolio from '../../hooks/usePortfolio';
+import PortfolioCard from '../PortfolioCard/PortfolioCard';
 import './Portfolio.css';
 
 const Portfolio = () => {
     const [portfolio, setPortfolio]=usePortfolio();
-    
+    console.log(portfolio)
+
     return (
         <div  className='portfolio-section'>
             <h3>Works we are proud of</h3>
             <p>Good Creation Systems Ltd is a fully Digital solution providing company incorporated and registered under the corporate affair commission in July 20, 2022.</p>
 
             <div className="sample-container">
-
+                <div className="left-container">
+                    left
+                </div>
+                <div className="right-container">
+                    {
+                        portfolio.map(singlePortfolio=><PortfolioCard 
+                            key={singlePortfolio.id}
+                            singlePortfolio={singlePortfolio}></PortfolioCard>)
+                    }
+                </div>
             </div>
         </div>
     );
---
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './PortfolioCard.css';
+
+const PortfolioCard = (props) => {
+    const {project_title, category, release_date, project_preview}=props.singlePortfolio;
+    return (
+        <div className='portfolio-card'>
+            <img src={project_preview} alt="Project Preview" />
+            <p>{category} | {release_date}</p>
+            <h4>{project_title}</h4>
+        </div>
+    );
+};
+
+export default PortfolioCard;
