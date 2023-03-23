3504faec6d28598cc34bebc4746f77610fa3c2ca
rirefat <rirefat.official@gmail.com>
2023-03-23 20:00:11 +0600

    services section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import HeroSection from './components/HeroSection/HeroSection';
 import Portfolio from './components/Portfolio/Portfolio';
+import Services from './components/Services/Services';
 
 const App = () => {
   return (
     <div className='container'>
       <HeroSection></HeroSection>
       <Portfolio></Portfolio>
+      <Services></Services>
     </div>
   );
 };
---
+++ src/components/Services/Services.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Services.css';
+
+const Services = () => {
+    return (
+        <div className='services-section'>
+            <h3>Our Services</h3>
+            <p>Here at Tridle we focus on markets where technology & capital can unlock long-term value and drive economic growth.</p>
+            <div className="services-container">
+                <div className="ui-ux">
+                    <h4>UI/UX Design</h4>
+                    <p>Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.</p>
+                </div>
+                <div className="web-dev">
+                    <h4>UI/UX Design</h4>
+                    <p>Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
