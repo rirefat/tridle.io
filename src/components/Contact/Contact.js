ba434756c9a797920b59add4a929336654b66a31
rirefat <rirefat.official@gmail.com>
2023-03-24 00:34:22 +0600

    done contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,6 +7,16 @@ const Contact = () => {
             <div className="contact-container">
                 <h3>Ready to get your own awesome website?</h3>
                 <p>Give us your phone number/email, and we will contact you to discuss the project</p>
+
+                <form className='contact-form'>
+                    <div>
+                        <input required className='name-field' type="text" placeholder='Your Name' />
+                        <input required className='email-number-field' type="text" name="email-phone" id="email_phone" placeholder='Email/ Phone' />
+                    </div>
+                    <input required className='description-field' type="text" name="description" id="description" placeholder='Tell us briefly about your project/company' />
+
+                    <input type="submit" value="Submit" className='submit-btn' />
+                </form>
             </div>
         </div>
     );
