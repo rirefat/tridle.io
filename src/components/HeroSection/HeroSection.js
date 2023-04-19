e5d5e77423f575d90bd8bfe4e4be5237ecabbace
rirefat <rirefat.official@gmail.com>
2023-04-19 16:04:38 +0600

    update call to action btn

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './HeroSection.css';
 
 import { AiOutlinePlayCircle } from 'react-icons/ai';
+import { BsArrowRight } from 'react-icons/bs';
 import { useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
     // Link Navigation function
@@ -14,11 +16,10 @@ const HeroSection = () => {
     return (
         <div className='hero-section'>
             <h1>We create the most <span className="highlighted">delightful</span> website for your brand</h1>
-            <p>Pick and personalize a cover – in hardback or softback – with any text you like, then select the paper type you want inside. Good news: we’ve got lined, dotted and plain.</p>
+            <p>Let us help you create an unforgettable digital presence for your brand.</p>
 
             <div className="call-to-action">
-                <button className="watch"><AiOutlinePlayCircle></AiOutlinePlayCircle>Watch us in action</button>
-                <button className="contact" onClick={()=>{handleNavigate('/home/#contact_us')}}>Get your own website</button>
+                <a href="#"><button>Get Your Own Website <span className="btn-icon"><BsArrowRight></BsArrowRight></span></button></a>
             </div>
         </div>
     );
