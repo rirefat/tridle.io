bfc02f1cdbc5a5570b61f0a9673d172a213a91d8
rirefat <rirefat.official@gmail.com>
2023-05-03 03:15:21 +0600

    solve bg issue of portfolio section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,26 +6,28 @@ import TopProject from '../TopProject/TopProject';
 import './Portfolio.css';
 
 const Portfolio = () => {
-    const [portfolio, setPortfolio]=usePortfolio();
-    const [project, setProject]=useTopProject();
+    const [portfolio, setPortfolio] = usePortfolio();
+    const [project, setProject] = useTopProject();
 
     return (
-        <div  className='portfolio-section' id='projects'>
-            <h3>Works we are proud of</h3>
-            <p className='sub-title'>Good Creation Systems Ltd is a fully Digital solution providing company incorporated and registered under the corporate affair commission in July 20, 2022.</p>
+        <div className="section-wrapper-secondary">
+            <div className='portfolio-section' id='projects'>
+                <h3>Works we are proud of</h3>
+                <p className='sub-title'>Good Creation Systems Ltd is a fully Digital solution providing company incorporated and registered under the corporate affair commission in July 20, 2022.</p>
 
-            <div className="sample-container">
-                <div className="left-container">
-                    {
-                        project.map(data=><TopProject key={data.id} data={data}></TopProject>)
-                    }
-                </div>
-                <div className="right-container">
-                    {
-                        portfolio.map(singlePortfolio=><PortfolioCard 
-                            key={singlePortfolio.id}
-                            singlePortfolio={singlePortfolio}></PortfolioCard>)
-                    }
+                <div className="sample-container">
+                    <div className="left-container">
+                        {
+                            project.map(data => <TopProject key={data.id} data={data}></TopProject>)
+                        }
+                    </div>
+                    <div className="right-container">
+                        {
+                            portfolio.map(singlePortfolio => <PortfolioCard
+                                key={singlePortfolio.id}
+                                singlePortfolio={singlePortfolio}></PortfolioCard>)
+                        }
+                    </div>
                 </div>
             </div>
         </div>
